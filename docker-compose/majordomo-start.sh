af97496a33c74dbf5619fbe50b9e7a4668607671
Dmitriy Studinskiy <dms@smartling.com>
2016-05-03 21:22:03 +0300

    Change script to use it in cron on reboot

--- docker-compose/majordomo-start.sh
+++ docker-compose/majordomo-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose -f /etc/majordomo/docker-compose.yml stop
-docker-compose -f /etc/majordomo/docker-compose.yml rm -f --all
-docker-compose -f /etc/majordomo/docker-compose.yml pull
-docker-compose -f /etc/majordomo/docker-compose.yml up -d
+/usr/local/bin/docker-compose -f /etc/majordomo/docker-compose.yml stop
+/usr/local/bin/docker-compose -f /etc/majordomo/docker-compose.yml rm -f --all
+/usr/local/bin/docker-compose -f /etc/majordomo/docker-compose.yml pull
+/usr/local/bin/docker-compose -f /etc/majordomo/docker-compose.yml up -d
