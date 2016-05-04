74946d9f3240ec47e79e921e31ebee4ca7d8e082
Dmitriy Studinskiy <dimitrystd@gmail.com>
2016-05-04 23:14:00 +0300

    Fixed generation of 000-default.conf

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sed "s/ServerName.*/ServerName ${VIRTUAL_HOST}/g" -i /etc/apache2/sites-available/000-default.conf
+
 /usr/sbin/apachectl start
 tail -f /var/log/apache2/error.log /var/log/php/error.log /var/www/debmes/*
