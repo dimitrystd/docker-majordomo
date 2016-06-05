17216754f342764be4ee37375d79ca9e459db5bf
Dmitriy Studinskiy <dimitrystd@gmail.com>
2016-06-05 17:06:07 +0300

    Fixed php warning from newrelic module

--- docker/files/install-newrelic.sh
+++ docker/files/install-newrelic.sh
@@ -4,5 +4,8 @@ echo "deb http://apt.newrelic.com/debian/ newrelic non-free" > /etc/apt/sources.
 export NR_INSTALL_SILENT=true
 apt-get update && apt-get install -y newrelic-php5
 newrelic-install install
+# Known issue https://discuss.newrelic.com/t/php-warning-module-newrelic-already-loaded-in-unknown-on-line-0/2903/19
+rm /etc/php5/cli/conf.d/newrelic.ini /etc/php5/apache2/conf.d/newrelic.ini
+
 apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
